4bf2b9f9d03676840216f3c687cc757554683e75
Aaron Suggs <aaron@ktheory.com>
2013-06-18 18:26:03 -0400

    Fix trim sorting

--- lib/egads/cli.rb
+++ lib/egads/cli.rb
@@ -173,12 +173,10 @@ module Egads
 
     desc "trim N", "[remote, plumbing] Deletes old releases, keeping the N most recent (by mtime)"
     def trim(n=4)
-      inside RemoteConfig.extract_to do
-        dirs = Dir.glob('*').sort_by{|path| File.mtime(path) }
-        dirs[n..-1].to_a.each do |dir|
-          say_status :trim, "Deleting #{dir}"
-          FileUtils.rm_rf(dir)
-        end
+      dirs = Dir.glob('*').sort_by{|path| File.mtime(path) }.reverse[n..-1].to_a
+      dirs.each do |dir|
+        say_status :trim, "Deleting #{dir}"
+        FileUtils.rm_rf(dir)
       end
     end
 
