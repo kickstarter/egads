daec13505595698bd6316dff456cb92f71aafb81
Aaron Suggs <aaron@ktheory.com>
2013-08-08 11:52:03 -0400

    extract: support patches and seed builds

--- lib/egads/command/extract.rb
+++ lib/egads/command/extract.rb
@@ -4,6 +4,7 @@ module Egads
 
     desc "[remote, plumbing] Downloads tarball for SHA from S3 and extracts it to the filesystem"
     class_option :force, type: :boolean, default: false, banner: "Overwrite existing files"
+    class_option :seed, type: :boolean, default: false, banner: "extract a seed build"
     argument :sha, type: :string, required: true, desc: 'git SHA to download and extract'
 
     def setup_environment
@@ -12,15 +13,15 @@ module Egads
 
     def download
       if should_download?
-        say_status :download, "Downloading tarball for #{sha}", :yellow
-        FileUtils.mkdir_p(release_dir)
+        say_status :download, "Downloading #{type} tarball for #{sha}", :yellow
+        FileUtils.mkdir_p(download_dir)
         duration = Benchmark.realtime do
           File.open(path, 'w') {|f| f << tarball.contents }
         end
         size = File.size(path)
         say_status :done, "Downloaded in %.1f seconds (%.1f KB/s)" % [duration, (size.to_f / 2**10) / duration]
       else
-        say_status :done, "Tarball already downloaded. Use --force to overwrite"
+        say_status :done, "#{type} tarball already downloaded. Use --force to overwrite"
       end
     end
 
@@ -28,8 +29,10 @@ module Egads
       # Check revision file to see if tarball is already extracted
       if should_extract?
         # Silence stderr warnings "Ignoring unknown extended header keyword"
-        # due to BSD/GNU tar.
+        # due to BSD/GNU tar differences.
         inside(release_dir) { run_with_code "tar -zxf #{path} 2>/dev/null" }
+        seed_sha = Pathname.new(release_dir).join("egads-seed").read.strip
+        invoke(Egads::Extract, [seed_sha], force: options[:force], seed: options[:seed])
       else
         say_status :done, "Tarball already extracted. Use --force to overwrite"
       end
@@ -44,12 +47,16 @@ module Egads
       RemoteConfig.release_dir(sha)
     end
 
+    def download_dir
+      options[:seed] ? RemoteConfig.seed_dir : release_dir
+    end
+
     def path
-      File.join(release_dir, "#{sha}.tar.gz")
+      File.join(download_dir, "#{sha}.tar.gz")
     end
 
     def tarball
-      @tarball ||= S3Tarball.new(sha, remote: true)
+      @tarball ||= S3Tarball.new(sha, remote: true, seed: options[:seed])
     end
 
     def should_download?
@@ -61,7 +68,11 @@ module Egads
     end
 
     def should_extract?
-      options[:force] || !File.exists?(extract_flag_path)
+      !options[:seed] && (options[:force] || !File.exists?(extract_flag_path))
+    end
+
+    def type
+      options[:seed] ? 'seed' : 'patch'
     end
 
   end
